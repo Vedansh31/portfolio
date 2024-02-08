4b7779a1be2b6a31b9a5fd31ac4bf5b69aa4d8f0
Vedansh Shekhar Ojha <shekharojhavso781@gmail.com>
2024-02-08 22:44:12 +0530

    Added a csv file for data intake and done added links to icons in contact section

--- server1.py
+++ server1.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -6,17 +7,33 @@ app = Flask(__name__)
 @app.route("/")
 def my_home():
     return render_template('index.html')
-
+# Dynamic Routing
 @app.route("/<page_name>")
 def any_page(page_name):
     return render_template(page_name)
+
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as my_db:
+#         name = data['name']
+#         e_mail = data['email']
+#         msg = data['message']
+#         file = my_db.write(f"\nname = {name} \ne-mail = {e_mail} \nmessage = {msg}")
+
+# Writing to a csv file
+def write_to_csv(data):
+    with open('database.csv', newline= '', mode= 'a') as my_db:
+        name = data['name']
+        e_mail = data['email']
+        msg = data['message']
+        file_csv = csv.writer(my_db, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)  
+        file_csv.writerow([name, e_mail, msg])
 
 # Taking Data from the User
 @app.route("/submit_form", methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_csv(data)
         return redirect('/tq.html')
     else:
-        return 'Try Again!!'
+        return 'Something went wrong, Try Again!'
